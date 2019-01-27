cc2f52f88bc52aca02344acb6f9d84b1e3d76a4d
Christian Gade <christian_gade@hotmail.com>
2019-01-27 15:36:53 +0100

    update repo

---
+++ leia/update_repo.bat
@@ -0,0 +1,10 @@
+git pull
+
+py update_repo.py repository.gade
+py update-directory-structure.py
+
+git add .
+git commit -a -m "update repo"
+git push
+
+pause
---
+++ leia/update_repo.old.bat
@@ -0,0 +1,4 @@
+py addons_xml_generator.py
+py update-directory-structure.py
+
+pause
